2071aaa4d2809e353a6a6299ecd11cf580346492
LBytes-32 <LukeBates20@gmail.com>
2023-09-26 22:59:54 -0400

    JSON response test

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,22 @@
-
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 
+#[tauri::command]
+fn retrieve_json_example() -> String {
+    return r#"
+        {
+            "name": "Luke Bates",
+            "age": 21,
+            "sex": "Male"
+        }
+    "#.to_string();
+}
+
 fn main() {
+    
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![retrieve_json_example])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { appWindow } from "@tauri-apps/api/window"
 import { Dom } from "./dom"
 import "./ui.css"
+import { invoke } from "@tauri-apps/api"
 
 let titlebar = Dom.createDivOn(document.body, {
   base: "title-bar",
@@ -11,6 +12,18 @@ let content = Dom.createDivOn(document.body, {
   base: "content",
   text: "hi"
 })
+
+type Person = {
+  name: string,
+  age: number,
+  sex: string
+}
+
+content.textContent = await invoke<string>('retrieve_json_example').then(value => {
+  let p = (JSON.parse(value) as Person)
+  return `I am ${p.name}. I am ${p.age} years old. I am ${p.sex}.`
+})
+
 
 let close = Dom.createDivOn(titlebar, {
   base: 'close-button',
